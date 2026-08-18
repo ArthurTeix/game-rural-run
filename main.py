e289c34295b9dccd1f1b1a56465ba1cead6dc831
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-08-17 22:45:58 -0300

    implementado o movimento do jogador

--- main.py
+++ main.py
@@ -10,11 +10,9 @@ tela = pygame.display.set_mode(tamanho_tela)
 fundo = pygame.image.load("assets/img/BackgroundSECO.png")
 
 # configurações do jogador
-# largura_jogador = 38
-# altura_jogador = 76
-largura_jogador = 76
-altura_jogador = 152
-jogador = pygame.Rect(231, 450, largura_jogador, altura_jogador)
+largura_jogador = 57
+altura_jogador = 114
+jogador = pygame.Rect(231, 470, largura_jogador, altura_jogador)
 
 # cores
 cores = {
@@ -29,15 +27,28 @@ pygame.display.set_caption("RURAL RUN")
 def desenhar_inicio_jogo():
     tela.blit(fundo, (0, 0))
 
+    # desenhando jogador na tela
     pygame.draw.rect(tela, cores['azul'], jogador)
+
+def movimento_jogador(evento):
+    if (evento.type == pygame.KEYDOWN): # se o evento for de pressionar tecla
+
+        if (evento.key == pygame.K_RIGHT) and (jogador.x < 549):
+            jogador.x += 1 
+
+        if (evento.key == pygame.K_LEFT) and (jogador.x > 0):
+            jogador.x -= 1
+        pass
 
 fim_de_jogo = False
 while not fim_de_jogo:
+    desenhar_inicio_jogo()
+
     for evento in pygame.event.get():
         if (evento.type == pygame.QUIT):
             fim_de_jogo = True 
 
-    desenhar_inicio_jogo()
+    movimento_jogador(evento)
 
     pygame.time.wait(1) 
     pygame.display.flip()
