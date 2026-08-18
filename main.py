217ade027aed335c586eb5c9901595a80a33122e
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-08-17 22:03:48 -0300

    adicionado fundo de tela inicial

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ pygame.init()
 tamanho_tela = (462, 606)
 tela = pygame.display.set_mode(tamanho_tela)
 
+# Carrega a imagem de fundo
+fundo = pygame.image.load("assets/img/BackgroundSECO.png")
+
 # cores
 cores = {
     'preto': (0, 0, 0)
@@ -15,13 +18,15 @@ cores = {
 pygame.display.set_caption("RURAL RUN")
 
 def desenhar_inicio_jogo():
-    tela.fill(cores['preto'])
+    tela.blit(fundo, (0, 0))
 
 fim_de_jogo = False
 while not fim_de_jogo:
     for evento in pygame.event.get():
         if (evento.type == pygame.QUIT):
             fim_de_jogo = True 
+
+    desenhar_inicio_jogo()
 
     pygame.time.wait(1) 
     pygame.display.flip()
