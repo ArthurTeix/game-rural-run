9da3933bc333c2aa77b47d08124f93e427da2684
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-08-18 17:07:13 -0300

    fundo dos obstaculos adicionados

--- main.py
+++ main.py
@@ -17,7 +17,6 @@ altura_jogador = 114
 jogador = pygame.Rect(231, 470, largura_jogador, altura_jogador)
 
 jogador_img = pygame.image.load("assets/img/Robozin.png").convert_alpha()
-
 # redimensiona a imagem pro tamanho do retângulo do jogador
 jogador_img = pygame.transform.scale(jogador_img, (largura_jogador, altura_jogador))
 
@@ -27,6 +26,9 @@ altura_obstaculo = 40
 velocidade_obstaculo = 1
 
 lista_obstaculos = []  # cada obstáculo é um pygame.Rect
+
+obstaculo_img = pygame.image.load("assets/img/obstaculos.png").convert_alpha()
+obstaculo_img = pygame.transform.scale(obstaculo_img, (largura_obstaculo, altura_obstaculo))
 
 # criar obstáculos periodicamente
 criar_obstaculos = pygame.USEREVENT + 1
@@ -42,6 +44,7 @@ cores = {
 # título do jogo
 pygame.display.set_caption("RURAL RUN")
 
+
 def desenhar_inicio_jogo():
     tela.blit(fundo, (0, 0))
 
@@ -50,21 +53,23 @@ def desenhar_inicio_jogo():
 
     # desenhando os obstáculos na tela
     for obstaculo in lista_obstaculos:
-        pygame.draw.rect(tela, cores['vermelho'], obstaculo)
+        tela.blit(obstaculo_img, obstaculo)
+
 
 def movimento_jogador(evento):
-    if (evento.type == pygame.KEYDOWN): # se o evento for de pressionar tecla
-
+    if (evento.type == pygame.KEYDOWN):
         if (evento.key == pygame.K_RIGHT) and (jogador.x < 400):
-            jogador.x += 1 
+            jogador.x += 10
 
         if (evento.key == pygame.K_LEFT) and (jogador.x > 3):
-            jogador.x -= 1
+            jogador.x -= 10
+
 
 def criar_obstaculo():
-    x = randint(30, 405) # obstaculos nascem aleatoriamente entre o px 30 e 405 de largura
+    x = randint(30, 405)  # obstaculos nascem aleatoriamente entre o px 30 e 405 de largura
     novo_obstaculo = pygame.Rect(x, -altura_obstaculo, largura_obstaculo, altura_obstaculo)
     lista_obstaculos.append(novo_obstaculo)
+
 
 def mover_obstaculos():
     for obstaculo in lista_obstaculos:
@@ -73,33 +78,35 @@ def mover_obstaculos():
     # remove obstáculos que já saíram da tela
     lista_obstaculos[:] = [obstaculo for obstaculo in lista_obstaculos if obstaculo.y < altura_tela]
 
-def verificar_colisao_o():
+
+def verificar_colisao_obstaculo():
     for obstaculo in lista_obstaculos:
         if jogador.colliderect(obstaculo):
             return True
     return False
+
 
 fim_de_jogo = False
 while not fim_de_jogo:
     desenhar_inicio_jogo()
 
     for evento in pygame.event.get():
-        if (evento.type == pygame.QUIT):
-            fim_de_jogo = True 
+        if evento.type == pygame.QUIT:
+            fim_de_jogo = True
 
-        if (evento.type == criar_obstaculos):
+        if evento.type == criar_obstaculos:
             quantidade_por_vez = 2
             for quant in range(quantidade_por_vez):
                 criar_obstaculo()
 
-    movimento_jogador(evento)
+        movimento_jogador(evento)
 
     mover_obstaculos()
 
-    if verificar_colisao_o():
+    if verificar_colisao_obstaculo():
         fim_de_jogo = True
 
-    pygame.time.wait(1) 
+    pygame.time.wait(1)
     pygame.display.flip()
 
 pygame.quit()
