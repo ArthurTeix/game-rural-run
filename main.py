1f59e83fa1efe3fc4fc9a71a0d92fe791c08fa1d
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-08-17 22:13:45 -0300

    implementando e testando jogador

--- main.py
+++ main.py
@@ -6,12 +6,21 @@ pygame.init()
 tamanho_tela = (462, 606)
 tela = pygame.display.set_mode(tamanho_tela)
 
-# Carrega a imagem de fundo
+# carrega a imagem de fundo
 fundo = pygame.image.load("assets/img/BackgroundSECO.png")
+
+# configurações do jogador
+# largura_jogador = 38
+# altura_jogador = 76
+largura_jogador = 76
+altura_jogador = 152
+jogador = pygame.Rect(231, 450, largura_jogador, altura_jogador)
 
 # cores
 cores = {
+    'azul': (0, 255, 0),
     'preto': (0, 0, 0)
+
 }
 
 # título do jogo
@@ -19,6 +28,8 @@ pygame.display.set_caption("RURAL RUN")
 
 def desenhar_inicio_jogo():
     tela.blit(fundo, (0, 0))
+
+    pygame.draw.rect(tela, cores['azul'], jogador)
 
 fim_de_jogo = False
 while not fim_de_jogo:
